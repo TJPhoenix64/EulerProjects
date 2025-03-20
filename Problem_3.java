191b0b247f1db598c1826d46fd9f1e2c8323dce5
TJPhoenix64 <Tyler.jordan@gcsaz.org>
2025-03-20 07:41:09 -0700

    Update Problem_3.java

--- Problem_3.java
+++ Problem_3.java
@@ -1,14 +1,33 @@
-
-public class Problem_3 {
-
+public class Problem_3 
+{
     public static void main(String[] args) {
         int answer = 0;
         long i;
-        for (i = 300425737572L; i > 0; i--) {
-            if (i % 1000000000 == 0) {
-                System.out.println(i);
+        long bigNumber = 600851475143L;
+        Long[] factorsOfBigNumber = new Long[10000];
+        int locationOfStuff = 0; 
+        for (i = 2; i < bigNumber / i; i++) {
+            if (bigNumber % i == 0) {
+                factorsOfBigNumber[locationOfStuff] = i;
+                locationOfStuff++;
+                factorsOfBigNumber[locationOfStuff] = bigNumber / i;
+                locationOfStuff++;
+                System.out.println(i + " " + bigNumber / i);
+            }
+        }
+        //finds the prime factors of the big number
+        for(int l = 0; l < locationOfStuff; l++){
+            for(int y = 0; y < factorsOfBigNumber[locationOfStuff]; y++){
+                int numOfNonFactors = 0;
+                if(factorsOfBigNumber[l] % y != 0){
+                    numOfNonFactors++;
+                }
+                if(factorsOfBigNumber[l] % y == 0){
+                    break;
+                }
             }
         }
         System.out.println("Yay! The answer is: " + answer);
+        
     }
 }
